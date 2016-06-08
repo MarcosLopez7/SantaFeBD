657df91d9146ebf45265a2efcd7854848fada93e
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-06-08 00:22:08 -0500

    setting db

--- SantaFeBD/settings.py
+++ SantaFeBD/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'SantaFeBD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'santafe',
+        'USER': 'vnpem',
+        'PASSWORD': '2664300191',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
